fca03350250463a27a38d0cc070405d831b2ba12
Murilo Manente <munente2002@gmail.com>
2022-09-13 08:52:47 -0300

    Feature: Changing connection string to remote SQL Server Instance

--- WebAPI/Models/Connection.cs
+++ WebAPI/Models/Connection.cs
@@ -2,6 +2,8 @@ namespace WebAPI.Models
 {
     public class Connection
     {
-        public static string ConnectionString = "Server=127.0.0.1,1433;Initial Catalog=people_crud;Persist Security Info=False;User Id=sa;Password=Passw0rd;Encrypt=True;TrustServerCertificate=True;";
+        public static string ConnectionString = "Server=database-1.c8paors2r1u4.us-east-1.rds.amazonaws.com,1433;Persist Security Info=True;Database=people_crud;User Id=admin;Password=teste123;Encrypt=True;TrustServerCertificate=True;";
     }
 }
+
+// Server=database-1.c8paors2r1u4.us-east-1.rds.amazonaws.com,1433;Database=people_crud;User Id=admin;Password=teste123;

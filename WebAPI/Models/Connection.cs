4d3d63c944f240c6c4ff6ebe9db5ce24d6ef411c
Murilo Manente <munente2002@gmail.com>
2022-09-13 08:55:54 -0300

    Bugfix: Renaming SQL Server instance and removing unnecessary comment

--- WebAPI/Models/Connection.cs
+++ WebAPI/Models/Connection.cs
@@ -2,8 +2,6 @@ namespace WebAPI.Models
 {
     public class Connection
     {
-        public static string ConnectionString = "Server=database-1.c8paors2r1u4.us-east-1.rds.amazonaws.com,1433;Persist Security Info=True;Database=people_crud;User Id=admin;Password=teste123;Encrypt=True;TrustServerCertificate=True;";
+        public static string ConnectionString = "Server=database-mssql.c8paors2r1u4.us-east-1.rds.amazonaws.com,1433;Persist Security Info=True;Database=people_crud;User Id=admin;Password=teste123;Encrypt=True;TrustServerCertificate=True;";
     }
 }
-
-// Server=database-1.c8paors2r1u4.us-east-1.rds.amazonaws.com,1433;Database=people_crud;User Id=admin;Password=teste123;
